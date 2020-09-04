499029af4798f6109d4069488ad23823441dee67
Kory Becker <kbecker@primaryobjects.com>
2020-09-04 12:11:50 -0400

    Simplified AI logic for loop detection. Added color encoding for knowledge UI.

--- js/managers/aiManager.js
+++ js/managers/aiManager.js
@@ -3,12 +3,14 @@ const AiManager = {
   path: [],
   foundLoop: false,
   foundWumpus: false,
+  recommendedMove: {},
 
-  initialize: (playerX, playerY, width, height) => {
+  initialize: (x, y, width, height) => {
     AiManager.knowledge = [];
     AiManager.path = [];
     AiManager.foundWumpus = false;
     AiManager.foundLoop = false;
+    AiManager.recommendedMove = {};
 
     for (let y=0; y<height; y++) {
       const row = [];
@@ -48,9 +50,9 @@ const AiManager = {
 
       // Choose the next move.
       // 1,2,3,2,1
-      const move = AiManager.move(x, y);
-      if (move) {
-        AiManager.path.push(`${move.x},${move.y}`);
+      AiManager.recommendedMove = AiManager.move(x, y);
+      /*if (AiManager.recommendedMove) {
+        AiManager.path.push(`${AiManager.recommendedMove.x},${AiManager.recommendedMove.y}`);
         const threshold = 4;
         if (AiManager.path.length > threshold) {
           // Check for a repeating loop by detecting a mirror pattern in the array.
@@ -88,9 +90,18 @@ const AiManager = {
             }
           }
         }
+      }*/
+
+      if (AiManager.recommendedMove.knowledge.visited > 1) {
+        !AiManager.foundLoop && console.log('Risky business!');
+        AiManager.foundLoop = true;
+      }
+      else if (!AiManager.recommendedMove.knowledge.visited) {
+        AiManager.foundLoop && console.log('Playing it safe.');
+        AiManager.foundLoop = false;
       }
 
-      return move;
+      return AiManager.recommendedMove;
     }
   },
 
@@ -319,14 +330,14 @@ const AiManager = {
     }
   },
 
-  toString: () => {
+  toString: (playerX, playerY) => {
     let result = '';
 
     for (let y=0; y < AiManager.knowledge.length; y++) {
       result += '| ';
 
       for (let x=0; x < AiManager.knowledge[y].length; x++) {
-        result += `v: ${AiManager.pad('    ', AiManager.knowledge[y][x].visited)} p: ${AiManager.pad('    ', AiManager.knowledge[y][x].pit)} w: ${AiManager.pad('    ', AiManager.knowledge[y][x].wumpus)} g: ${AiManager.pad('    ', AiManager.knowledge[y][x].gold)} | `;
+        result += `${AiManager.knowledge[y][x].pit >= 0.5 ? '@@@' : ''}${AiManager.knowledge[y][x].pit === 0.25 ? '@' : ''}${(x === AiManager.recommendedMove.x && y === AiManager.recommendedMove.y) ? '$' : ''}${(x === playerX && y === playerY) ? '*' : ''} v: ${AiManager.pad('    ', AiManager.knowledge[y][x].visited)} p: ${AiManager.pad('    ', AiManager.knowledge[y][x].pit)} w: ${AiManager.pad('    ', AiManager.knowledge[y][x].wumpus)} g: ${AiManager.pad('    ', AiManager.knowledge[y][x].gold)} ${x === AiManager.recommendedMove.x && y === AiManager.recommendedMove.y ? '$$' : ''}${x === playerX && y === playerY ? '**' : ''}${AiManager.knowledge[y][x].pit >= 0.5 ? '@@@@' : ''}${AiManager.knowledge[y][x].pit === 0.25 ? '@@' : ''}| `;
       }
       result += '\n';
     }
