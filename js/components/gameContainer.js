4c124ba9aa54ca84da8532356a979fa2ba763704
Kory Becker <kbecker@primaryobjects.com>
2020-09-04 15:00:37 -0400

    Style.

--- js/components/gameContainer.js
+++ js/components/gameContainer.js
@@ -71,39 +71,46 @@ class GameContainer extends React.Component {
         <Game width={ this.state.width } height={ this.state.height } arrows={ this.state.arrows } arrowState={ this.state.arrowState } updateArrows={ this.onUpdateArrows } cheatMode={ this.state.cheat } reset={ this.state.reset } updateKnowledge={ this.onKnowledge }></Game>
 
         <div class="gamePlayOptions mt-3">
-            <div class='row'>
-              <div class='col text-muted'>
-                Grid Size
+          <div class='row'>
+            <div class='col text-muted'>
+              Grid Size
+            </div>
+          </div>
+          <div class='row no-gutters'>
+            <div class='col-auto'>
+              <input type="number" id="width" name="width" min="3" value={ this.state.width } onChange={ this.onWidth }/>
+            </div>
+            <div class='col-auto'>
+              <input type="number" id="height" name="height" min="3" value={ this.state.height } onChange={ this.onHeight }/>
+            </div>
+            <div class='col-auto'>
+              <input type="button" id="reset" name="reset" value="Reset" onClick={ this.onReset }/>
+            </div>
+            <div class='col-auto'>
+              <button type="button" id="btn-shoot" class={ `btn btn-${ this.state.arrows ? 'primary' : 'secondary disabled' } btn-sm ${ this.state.arrowState === WumpusManager.constants.arrowState.armed ? 'active' : ''}` } data-toggle="button" aria-pressed="false" autocomplete="off" onClick={ this.onShoot }>
+                <i class="fas fa-bullseye mr-1" />
+                Shoot <span class="badge badge-light ml-1 mr-0">{this.state.arrows}</span>
+              </button>
+            </div>
+            <div class='col-auto'>
+              <button type="button" class="btn btn-secondary btn-sm" data-toggle="button" aria-pressed="false" autocomplete="off" onClick={ this.onCheat }>
+                <i class="fas fa-theater-masks mr-1" />
+                Cheat
+              </button>
+            </div>
+          </div>
+          <div id='knowledgebase' class='row no-guggers'>
+            <div class='col-auto'>
+              <div class='panel panel-default'>
+                <div class='panel-heading collapsed' data-toggle='collapse' data-target='#knowledgebaseContainer'>
+                  <i class='fa fa-chevron fa-fw' ></i> AI Knowledge
+                </div>
+                <div id='knowledgebaseContainer' class='collapse in'>
+                  <div id='knowledge' dangerouslySetInnerHTML={{__html: this.state.knowledge ? this.state.knowledge.replace(/\n/g, '<br>').replace(/ /g, '&nbsp;').replace('**', '</span>').replace('*', '<span style="color: black;">').replace('$$', '</span>').replace('$', '<span style="color: limegreen;">').replace(/@@@@/g, '</span>').replace(/@@@/g, '<span style="color: red; opacity: .9">').replace(/@@/g, '</span>').replace(/@/g, '<span style="color: red; opacity: .5">').replace(/\^\^\^\^/g, '</span>').replace(/\^\^\^/g, '<span style="color: red; opacity: .9">').replace(/\^\^/g, '</span>').replace(/\^/g, '<span style="color: red; opacity: .5">') : '' }}></div>
+                </div>
               </div>
             </div>
-            <div class='row no-gutters'>
-              <div class='col-auto'>
-                <input type="number" id="width" name="width" min="3" value={ this.state.width } onChange={ this.onWidth }/>
-              </div>
-              <div class='col-auto'>
-                <input type="number" id="height" name="height" min="3" value={ this.state.height } onChange={ this.onHeight }/>
-              </div>
-              <div class='col-auto'>
-                <input type="button" id="reset" name="reset" value="Reset" onClick={ this.onReset }/>
-              </div>
-              <div class='col-auto'>
-                <button type="button" id="btn-shoot" class={ `btn btn-${ this.state.arrows ? 'primary' : 'secondary disabled' } btn-sm ${ this.state.arrowState === WumpusManager.constants.arrowState.armed ? 'active' : ''}` } data-toggle="button" aria-pressed="false" autocomplete="off" onClick={ this.onShoot }>
-                  <i class="fas fa-bullseye mr-1" />
-                  Shoot <span class="badge badge-light ml-1 mr-0">{this.state.arrows}</span>
-                </button>
-              </div>
-              <div class='col-auto'>
-                <button type="button" class="btn btn-secondary btn-sm" data-toggle="button" aria-pressed="false" autocomplete="off" onClick={ this.onCheat }>
-                  <i class="fas fa-theater-masks mr-1" />
-                  Cheat
-                </button>
-              </div>
-            </div>
-            <div class='row no-guggers'>
-              <div class='col-auto'>
-                <div id='knowledge' dangerouslySetInnerHTML={{__html: this.state.knowledge ? this.state.knowledge.replace(/\n/g, '<br>').replace(/ /g, '&nbsp;').replace('**', '</span>').replace('*', '<span style="color: black;">').replace('$$', '</span>').replace('$', '<span style="color: limegreen;">').replace(/@@@@/g, '</span>').replace(/@@@/g, '<span style="color: red; opacity: .9">').replace(/@@/g, '</span>').replace(/@/g, '<span style="color: red; opacity: .5">').replace(/\^\^\^\^/g, '</span>').replace(/\^\^\^/g, '<span style="color: red; opacity: .9">').replace(/\^\^/g, '</span>').replace(/\^/g, '<span style="color: red; opacity: .5">') : '' }}></div>
-              </div>
-            </div>
+          </div>
         </div>
       </div>
     );
